f5d3f1e54e9487f86d78b74982e9c2245d0882e1
Scripthqs <1941186772@qq.com>
2021-12-30 21:44:24 +0800

    login

---
+++ src/views/Login.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="login_container">
+    <div class="background">
+      <div class="shape"></div>
+      <div class="shape"></div>
+    </div>
+    <form class="login_form">
+      <h3>请登录</h3>
+      <label for="username">账号</label>
+      <input type="text" v-model="name" placeholder="请输入账号" @input="nameInput">
+      <span v-if="error.name" class="err-msg">{{error.name}}</span>
+
+      <label for="password">密码</label>
+      <input type="text" v-model="pwd" placeholder="请输入密码" @input="pwdInput">
+      <span v-if="error.pwd" class="err-msg">{{error.pwd}}</span>
+
+      <button @click="login">登录</button>
+    </form>
+    <div v-show="isShow" class="toast">{{message}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      name: 'admin',
+      pwd: '1234Abc!',
+      error: {
+        name: '',
+        pwd: ''
+      },
+      message: '',
+      isShow: false
+    }
+  },
+  methods: {
+    check (name, pwd) {
+      if (!name && !pwd) {
+        this.error.name = '请输入账号'
+        this.error.pwd = '请输入密码'
+        return false
+      }
+      if (!name) {
+        this.error.name = '请输入账号'
+        return false
+      }
+      if (!pwd) {
+        this.error.pwd = '请输入密码'
+        return false
+      }
+      if (pwd) {
+        const reg = /^(?![a-zA-Z]+$)(?![A-Z0-9]+$)(?![A-Z\W_]+$)(?![a-z0-9]+$)(?![a-z\W_]+$)(?![0-9\W_]+$)[a-zA-Z0-9\W_]{8,}$/
+        // console.log(reg.test(pwd))
+        if (!reg.test(pwd)) { this.error.pwd = '密码必须包含大写、小写、数字、特殊字符其三，并且位数不少于8位' }
+        return reg.test(pwd)
+      }
+    },
+    login () {
+      // console.log(this)
+      const { name, pwd, $router, check } = this
+      // if (!this.check(name, pwd)) return
+      // console.log(name)
+      // console.log(pwd)
+
+      // 对密码进行预校验
+      if (check(name, pwd)) {
+        // 判断密码
+        if (name === 'admin' && pwd === '1234Abc!') {
+          setTimeout(() => {
+            $router.push('/home')
+          }, 1000)
+        } else {
+          this.isShow = true
+          this.message = '登录失败，账号或密码错误！'
+          setTimeout(() => {
+            this.isShow = false
+            this.message = ''
+          }, 3000)
+          // alert('账号或密码错误')
+        }
+      }
+    },
+    nameInput () {
+      if (this.name) {
+        this.error.name = false
+      }
+    },
+    pwdInput () {
+      if (this.pwd) {
+        this.error.pwd = false
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="less">
+.login_container {
+  height: 100%;
+  background-color: #080710;
+  .background {
+    width: 430px;
+    height: 520px;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+    .shape {
+      height: 200px;
+      width: 200px;
+      position: absolute;
+      border-radius: 50%;
+    }
+  }
+  .shape:first-child {
+    background: linear-gradient(#1845ad, #23a2f6);
+    left: -80px;
+    top: -80px;
+  }
+  .shape:last-child {
+    background: linear-gradient(to right, #ff512f, #f09819);
+    right: -30px;
+    bottom: -80px;
+  }
+  .login_form * {
+    font-family: sans-serif;
+    color: #ffffff;
+    letter-spacing: 0.5px;
+    outline: none;
+    border: none;
+  }
+  .login_form {
+    height: 520px;
+    width: 400px;
+    background-color: rgba(255, 255, 255, 0.13);
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 10px;
+    backdrop-filter: blur(10px);
+    border: 2px solid rgba(255, 255, 255, 0.1);
+    box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+    padding: 50px 35px;
+    h3 {
+      font-size: 32px;
+      font-weight: 500;
+      line-height: 42px;
+      text-align: center;
+    }
+    label {
+      display: block;
+      text-align: left;
+      margin-top: 30px;
+      font-size: 16px;
+      font-weight: 500;
+    }
+    input {
+      display: block;
+      height: 50px;
+      width: 100%;
+      background-color: rgba(255, 255, 255, 0.07);
+      border-radius: 3px;
+      padding: 0 10px;
+      margin-top: 8px;
+      font-size: 14px;
+      font-weight: 300;
+    }
+    ::placeholder {
+      color: #e5e5e5;
+    }
+    button {
+      margin-top: 50px;
+      width: 100%;
+      background-color: #ffffff;
+      color: #080710;
+      padding: 15px 0;
+      font-size: 18px;
+      font-weight: 600;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+    .err-msg {
+      color: yellow;
+      position: absolute;
+      left: 10%;
+      right: 10%;
+      text-align: left;
+    }
+  }
+  .toast {
+    position: fixed;
+    // background-color: #fff;
+    // border: 1px solid red;
+    border-radius: 5px;
+    font-size: 24px;
+    color: red;
+    left: 50%;
+    top: 8%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
